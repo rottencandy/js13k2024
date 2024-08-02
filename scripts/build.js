6966b5daa96c7e4f39b56b88988e2e6bf859f5c1
saud <md.saud020@gmail.com>
2024-08-03 00:38:05 +0530

    format support scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,37 +1,39 @@
-import { htmlPlugin } from '@craftamap/esbuild-plugin-html';
-import esbuild from 'esbuild';
+import { htmlPlugin } from "@craftamap/esbuild-plugin-html"
+import esbuild from "esbuild"
 
 const ctx = await esbuild.context({
-    entryPoints: ['src/main.ts', 'src/app.css'],
+    entryPoints: ["src/main.ts", "src/app.css"],
     bundle: true,
     metafile: true,
-    charset: 'utf8',
-    format: 'iife',
-    outdir: 'app',
-    loader: { '.png': 'dataurl' },
-    assetNames: '[name]',
+    charset: "utf8",
+    format: "iife",
+    outdir: "app",
+    loader: { ".png": "dataurl" },
+    assetNames: "[name]",
     plugins: [
         htmlPlugin({
-            files: [{
-                filename: 'index.html',
-                entryPoints: ['src/main.ts', 'src/app.css'],
-                title: 'ENGINE',
-                inline: {
-                    js: true,
-                    css: true,
+            files: [
+                {
+                    filename: "index.html",
+                    entryPoints: ["src/main.ts", "src/app.css"],
+                    title: "ENGINE",
+                    inline: {
+                        js: true,
+                        css: true,
+                    },
+                    htmlTemplate: "src/index.html",
                 },
-                htmlTemplate: 'src/index.html',
-            }],
+            ],
         }),
     ],
     minify: true,
     mangleProps: /_$/,
-});
+})
 
 try {
-    await ctx.rebuild();
+    await ctx.rebuild()
 } catch (e) {
-    console.error('Error: ', e);
-    process.exit(1);
+    console.error("Error: ", e)
+    process.exit(1)
 }
-process.exit(0);
+process.exit(0)
--- scripts/dev.js
+++ scripts/dev.js
@@ -1,33 +1,35 @@
-import { htmlPlugin } from '@craftamap/esbuild-plugin-html';
-import esbuild from 'esbuild';
+import { htmlPlugin } from "@craftamap/esbuild-plugin-html"
+import esbuild from "esbuild"
 
 const ctx = await esbuild.context({
-    entryPoints: ['src/main.ts', 'src/app.css'],
+    entryPoints: ["src/main.ts", "src/app.css"],
     bundle: true,
     metafile: true,
-    charset: 'utf8',
-    format: 'iife',
-    outdir: 'app',
-    loader: { '.png': 'dataurl' },
-    assetNames: '[name]',
+    charset: "utf8",
+    format: "iife",
+    outdir: "app",
+    loader: { ".png": "dataurl" },
+    assetNames: "[name]",
     plugins: [
         htmlPlugin({
-            files: [{
-                filename: 'index.html',
-                entryPoints: ['src/main.ts', 'src/app.css'],
-                title: 'ENGINE',
-                htmlTemplate: 'src/index.html',
-            }],
+            files: [
+                {
+                    filename: "index.html",
+                    entryPoints: ["src/main.ts", "src/app.css"],
+                    title: "ENGINE",
+                    htmlTemplate: "src/index.html",
+                },
+            ],
         }),
     ],
-    sourcemap: 'inline',
-});
+    sourcemap: "inline",
+})
 
-await ctx.watch();
-const { host, port } = await ctx.serve({ servedir: 'app' });
-console.log(`Serving: http://${host}:${port}`);
+await ctx.watch()
+const { host, port } = await ctx.serve({ servedir: "app" })
+console.log(`Serving: http://${host}:${port}`)
 
-process.on('SIGINT', async () => {
-    await ctx.dispose();
-    process.exit();
-});
+process.on("SIGINT", async () => {
+    await ctx.dispose()
+    process.exit()
+})
