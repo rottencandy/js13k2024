e18cb4bacf87c60970424cb33f05e807307c9474
saud <md.saud020@gmail.com>
2024-08-25 22:39:06 +0530

    add button

--- src/core/math.ts
+++ src/core/math.ts
@@ -30,6 +30,15 @@ export const aabb = (
     h2: number,
 ) => x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2
 
+export const pointInRect = (
+    px: number,
+    py: number,
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+) => px > x && py > x && px < x + w && py < y + h
+
 export const ccCollision = (
     x1: number,
     y1: number,
--- src/ui.ts
+++ src/ui.ts
@@ -1,14 +1,14 @@
-import { CTX } from "./core/canvas"
+import { HEIGHT, WIDTH } from "./const"
+import { type CTX } from "./core/canvas"
 import { renderFont } from "./core/font"
 import { keys } from "./core/input"
+import { pointInRect } from "./core/math"
 import { scene, Scene, startGame } from "./scene"
 
 export const updateUI = (dt: number) => {
     switch (scene) {
         case Scene.title:
-            if (keys.btnp.clk) {
-                startGame()
-            }
+            startButton.update()
             break
         case Scene.gameplay:
             break
@@ -23,8 +23,11 @@ export const updateUI = (dt: number) => {
 export const renderUI = (ctx: CTX) => {
     switch (scene) {
         case Scene.title:
+            ctx.fillStyle = "brown"
+            startButton.render(ctx)
             ctx.fillStyle = "pink"
-            renderFont(ctx, "TEST", 10, 100, 200)
+            renderFont(ctx, "TITLE", 10, 100, 200)
+            renderFont(ctx, "START", 10, 200, 400)
             break
         case Scene.gameplay:
             break
@@ -34,3 +37,27 @@ export const renderUI = (ctx: CTX) => {
             break
     }
 }
+
+const btn = (
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    onClick: () => void,
+) => {
+    return {
+        update: () => {
+            if (
+                keys.btnp.clk &&
+                pointInRect(keys.ptr.x * WIDTH, keys.ptr.y * HEIGHT, x, y, w, h)
+            ) {
+                onClick()
+            }
+        },
+        render: (ctx: CTX) => {
+            ctx.fillRect(x, y, w, h)
+        },
+    }
+}
+
+const startButton = btn(180, 380, 220, 90, startGame)
