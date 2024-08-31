9034e1b7f40cff581a7de7e7c595e52c1bc7aaf8
saud <md.saud020@gmail.com>
2024-08-31 14:38:49 +0530

    use gl constants

--- src/core/post-process.ts
+++ src/core/post-process.ts
@@ -1,4 +1,23 @@
 import { resize } from "./canvas"
+import {
+    GL_ARRAY_BUFFER,
+    GL_BLEND,
+    GL_COLOR_BUFFER_BIT,
+    GL_FLOAT,
+    GL_FRAGMENT_SHADER,
+    GL_LINK_STATUS,
+    GL_NEAREST,
+    GL_ONE_MINUS_SRC_ALPHA,
+    GL_RGBA,
+    GL_SRC_ALPHA,
+    GL_STATIC_DRAW,
+    GL_TEXTURE_2D,
+    GL_TEXTURE_MAG_FILTER,
+    GL_TEXTURE_MIN_FILTER,
+    GL_TRIANGLES,
+    GL_UNSIGNED_BYTE,
+    GL_VERTEX_SHADER,
+} from "./gl-constants"
 
 // prettier-ignore
 const vertShader = `#version 300 es
@@ -33,15 +52,15 @@ export const setupPostProcess = (
     canvas.width = width
     canvas.height = height
     gl.viewport(0, 0, width, height)
-    gl.enable(gl.BLEND)
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+    gl.enable(GL_BLEND)
+    gl.blendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
     const vao = gl.createVertexArray()!
     const buf = gl.createBuffer()!
     const tex = gl.createTexture()!
     const prg = gl.createProgram()!
-    const vert = gl.createShader(gl.VERTEX_SHADER)!
-    const frag = gl.createShader(gl.FRAGMENT_SHADER)!
+    const vert = gl.createShader(GL_VERTEX_SHADER)!
+    const frag = gl.createShader(GL_FRAGMENT_SHADER)!
 
     gl.shaderSource(vert, vertShader)
     gl.shaderSource(frag, fragShader)
@@ -50,7 +69,7 @@ export const setupPostProcess = (
     gl.attachShader(prg, vert)
     gl.attachShader(prg, frag)
     gl.linkProgram(prg)
-    if (!gl.getProgramParameter(prg, gl.LINK_STATUS)) {
+    if (!gl.getProgramParameter(prg, GL_LINK_STATUS)) {
         console.error("Failed: " + gl.getProgramInfoLog(prg))
         console.error("Vert: " + gl.getShaderInfoLog(vert))
         console.error("Frag: " + gl.getShaderInfoLog(frag))
@@ -59,9 +78,9 @@ export const setupPostProcess = (
 
     gl.bindVertexArray(vao)
     gl.enableVertexAttribArray(0)
-    gl.bindBuffer(gl.ARRAY_BUFFER, buf)
+    gl.bindBuffer(GL_ARRAY_BUFFER, buf)
     gl.bufferData(
-        gl.ARRAY_BUFFER,
+        GL_ARRAY_BUFFER,
         // prettier-ignore
         new Float32Array([
             0, 0,
@@ -71,25 +90,25 @@ export const setupPostProcess = (
             1, 1,
             0, 1,
         ]),
-        gl.STATIC_DRAW,
+        GL_STATIC_DRAW,
     )
-    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0)
+    gl.vertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0)
 
-    gl.bindTexture(gl.TEXTURE_2D, tex)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.bindTexture(GL_TEXTURE_2D, tex)
+    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
 
     return (ctx: CanvasRenderingContext2D) => {
         gl.texImage2D(
-            gl.TEXTURE_2D,
+            GL_TEXTURE_2D,
             0,
-            gl.RGBA,
-            gl.RGBA,
-            gl.UNSIGNED_BYTE,
+            GL_RGBA,
+            GL_RGBA,
+            GL_UNSIGNED_BYTE,
             ctx.canvas,
         )
         gl.clearColor(0.3, 0.3, 0.3, 1)
-        gl.clear(gl.COLOR_BUFFER_BIT)
-        gl.drawArrays(gl.TRIANGLES, 0, 6)
+        gl.clear(GL_COLOR_BUFFER_BIT)
+        gl.drawArrays(GL_TRIANGLES, 0, 6)
     }
 }
